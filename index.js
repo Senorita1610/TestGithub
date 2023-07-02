7d4529a68a3424ef39657848c4ded8b2ff7077ce
senorita <kainnoowa2303@gmail.com>
2023-07-02 09:09:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// function print_message(){
+//     "use strict";
+// }
+// print_message();
+
+// "use strict";
+
+// document.getElementById("content").innerHTML = "Học lập trình JavaScript";
+// console.log("Hello world");
+// alert("Hello world");
+
+// message = "Học lập trình JavaScript";
+// document.write(message);
+
+// let x=1,y=2,z=3
+// console.log(x);
+// console.log(y);
+// console.log(z);
+let x=67;
+console.log(0/0);
+console.log("10"/"2");
